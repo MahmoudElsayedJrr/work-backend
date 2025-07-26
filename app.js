a6d41883973b16728ca9f58669d6b6a3b099dd6d
Mahmoud Elsayed <mr.houda2001@gmail.com>
2025-07-26 13:39:14 +0300

    add cors

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "*" }));
 app.use(express.json());
+
+app.use(cors());
 
 connectDB();
 
